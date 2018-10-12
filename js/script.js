b25cd5d02d2204e114af0ace149acf04fe75ab79
P40\lenovo <shengbangshenghua@shengbangshenghua.com>
2018-10-12 09:27:57 +0800

    待完善添加新闻事件

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-/**
- * Created by lenovo on 18/10/12 012.
- */
+var vm = new Vue({
+    el:"#app",
+    data:{
+        news:[
+            {"title":"新闻标题1","content":"新闻的内容1","sort":"科技","name":"人民网","time":"时间戳","update":"更新的时间戳","state":true},
+            {"title":"新闻标题2","content":"新闻的内容2","sort":"国内","name":"人民网","time":"时间戳","update":"更新的时间戳","state":true},
+            {"title":"新闻标题3","content":"新闻的内容3","sort":"科技","name":"人民网","time":"时间戳","update":"更新的时间戳","state":true},
+            {"title":"新闻标题4","content":"新闻的内容4","sort":"军事","name":"人民网","time":"时间戳","update":"更新的时间戳","state":true},
+            {"title":"新闻标题5","content":"新闻的内容5","sort":"科技","name":"人民网","time":"时间戳","update":"更新的时间戳","state":true}
+        ]
+    }
+})
